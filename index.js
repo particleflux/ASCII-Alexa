5d05ca1ba5bb73e7334b0c8e42286af25c7f2729
particleflux <particleflux@gmail.com>
2017-07-23 22:16:26 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Alexa = require('alexa-sdk');
+const APP_ID = 'amzn1.ask.skill.fa724d1b-4759-4065-90de-6e979a74593b';
+const SKILL_NAME = 'ASCII';
+
+exports.handler = function(event, context, callback) {
+    let alexa = Alexa.handler(event, context, callback);
+    alexa.appId = APP_ID;
+
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+let handlers = {
+    'LaunchRequest': function () {
+        this.emit(':tell', "Entschuldigung, es fehlt ein Buchstabe");
+    },
+    'ConvertCharIntent': function () {
+        this.emit('ConvertChar');
+    },
+    'ConvertChar': function () {
+        let slotValue = this.event.request.intent.slots.character.value;
+        if (!slotValue || !slotValue.length) {
+            // error
+            this.emit(':tell', "Entschuldigung, es fehlt ein Buchstabe");
+        }
+
+        let code = slotValue.charCodeAt(0);
+
+        let output = slotValue[0] + " entspricht ASCII " + code;
+        this.emit(':tellWithCard', output, SKILL_NAME, output);
+    }
+};
---
+++ test/index.test.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const alexaTest = require('alexa-skill-test-framework');
+const myHandler = require('../index');
+
+alexaTest.initialize(
+    myHandler,
+    "amzn1.ask.skill.fa724d1b-4759-4065-90de-6e979a74593b",
+    "amzn1.ask.account.AM3B227HF3FAM1B261HK7FFM3A2"
+);
+
+describe("direct launch with char", function () {
+
+    [
+        {
+            'char': 'a',
+            'response': '97'
+        },
+        {
+            'char': 'z',
+            'response': '122'
+        }
+    ].forEach(function (data) {
+        alexaTest.test([
+            {
+                request: alexaTest.getIntentRequest('ConvertCharIntent', {
+                    "character": data.char
+                }),
+                says: data.char + ' entspricht ASCII ' + data.response,
+                shouldEndSession: true
+            }
+        ]);
+    });
+});
+
+
+describe("direct launch without char", function () {
+
+    alexaTest.test([
+        {
+            request: alexaTest.getIntentRequest('ConvertCharIntent', {
+                "character": ''
+            }),
+            says: "Entschuldigung, es fehlt ein Buchstabe",
+            shouldEndSession: true
+        }
+    ]);
+});
+
+describe("LaunchRequest without char", function () {
+    alexaTest.test([
+        {
+            request: alexaTest.getIntentRequest('LaunchRequest'),
+            says: "Entschuldigung, es fehlt ein Buchstabe",
+            shouldEndSession: true
+        }
+    ]);
+});
